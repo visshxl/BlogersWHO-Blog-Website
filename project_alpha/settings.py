1541954f2f8c8492a9fe18fe3e5dd9ef599c6e5f
vishal chauhan <vc989021@gmail.com>
2025-01-03 15:58:54 +0530

    Host

--- project_alpha/settings.py
+++ project_alpha/settings.py
@@ -157,6 +157,7 @@ AWS_S3_FILE_OVERWRITE = False
 
 
 
+
 # Django < 4.2
 
 '''
@@ -167,7 +168,6 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 '''
 
 # Django 4.2 >
-
 
 STORAGES = {
     # Media file (image) management
