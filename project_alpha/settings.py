31dd7d317a1f86ad55f61fdb33c34e354bea4617
vishal chauhan <vc989021@gmail.com>
2025-02-05 14:02:14 +0530

    Superuser

--- project_alpha/settings.py
+++ project_alpha/settings.py
@@ -76,18 +76,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'project_alpha.wsgi.application'
 
+ 
+# new
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-'''
 
 
 # Render postgre database
